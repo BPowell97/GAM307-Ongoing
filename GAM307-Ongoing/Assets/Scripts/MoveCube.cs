055c184f6730ef482cd7c1f83158f97b79fd036f
Benjamin Powell <BPowell2@myjmc.edu.au>
2018-09-25 12:00:31 +1000

    Move Cube Script

---
+++ GAM307-Ongoing/Assets/Scripts/MoveCube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+
+    //public GameObject cube;
+    public float speed = 5f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        int rnd = Random.Range(0, 360);
+        transform.Rotate(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360)));  
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+       if(Input.GetKey(KeyCode.W))
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+    }
+}
